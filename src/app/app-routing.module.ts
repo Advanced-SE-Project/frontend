b78379cf343f5c6e7fc613505b12c367a0140a4e
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-10-14 16:19:10 +0200

    component creation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  // Add other routes here
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
