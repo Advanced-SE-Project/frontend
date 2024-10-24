744b4f8134d5dd85928637459725daf5ce6386bb
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-10-25 01:15:30 +0200

    analytics component with hardcoded data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), 
+    // Include BrowserAnimationsModule for animations
+    importProvidersFrom(BrowserAnimationsModule),
+
+    // Keep the ng2-charts providers
+    provideCharts(withDefaultRegisterables()),
+  ],
+}).catch((err) => console.error(err));
