e4d939416e9abfed3c7c869bdf67af1825a03ad1
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-11-03 23:30:05 +0100

    Sidebar Component Decoupling

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
   imports: [
     CommonModule, 
-    RouterModule
+    RouterModule,
+    SidebarComponent
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
