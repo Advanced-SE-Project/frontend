4a8cd3fa8f1dfc6e6d7e60985e3c8ec345c8f097
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-11-03 23:22:57 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,22 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
-import { SupportComponent } from './components/support/support.component';
 import { SettingsComponent } from './components/settings/settings.component';
-import { LogInComponent } from './components/log-in/log-in.component';
+import { SupportComponent } from './components/support/support.component';
 
 export const routes: Routes = [
+  { path: '', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent},
   { path: 'transactions', component: TransactionsComponent },
   { path: 'analytics', component: AnalyticsComponent },
   { path: 'support', component: SupportComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'log-in', component: LogInComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: '/dashboard' } // Wildcard route for unknown paths
+  { path: 'create-account', component: CreateAccountComponent},
+  { path: '**', redirectTo: '/about' } // Fallback for unknown routes
 ];
