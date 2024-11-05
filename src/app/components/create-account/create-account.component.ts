5645ef8777510f283c3b876fa75161fa16d97cb7
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-11-05 11:03:14 +0100

    Add standalone create-account component with UI

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
   imports: [
-    CommonModule, 
-    RouterModule
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   templateUrl: './create-account.component.html',
-  styleUrl: './create-account.component.scss'
+  styleUrls: ['./create-account.component.scss']
 })
 export class CreateAccountComponent {
+  user = { username: '', password: '', confirmPassword: '' };
 
+  constructor(private router: Router) {}
+
+  createAccount() {
+    if (this.user.password === this.user.confirmPassword) {
+      console.log('Account created:', this.user);
+      this.router.navigate(['/login']);
+    } else {
+      console.error('Passwords do not match');
+    }
+  }
 }
