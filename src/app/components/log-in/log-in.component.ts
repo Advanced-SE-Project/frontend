b3d138aeb6cb20fa64086a78e275b17a44263055
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-11-05 10:48:21 +0100

    add standalone login component with basic login/logout functionality and basic style

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
   imports: [
     CommonModule, 
-    RouterModule
+    RouterModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
-export class LogInComponent {
 
+export class LoginComponent {
+  user = { username: '', password: '' };
+
+  constructor(private router: Router) {}
+
+  login() {
+    console.log('User logged in:', this.user);
+    this.router.navigate(['/dashboard']);
+  }
+
+  logout() {
+    console.log('User logged out');
+    this.router.navigate(['/about']);
+  }
 }
