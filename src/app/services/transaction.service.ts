eff2f92268cebc6384c4330103c7a57dbec536b3
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2024-12-12 11:33:20 +0100

    api update

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class TransactionService {
-  private baseUrl = 'http://localhost:3000/api/transactions';
+  private baseUrl = 'http://localhost:5000/transaction-service/api/transactions';
 
   constructor(private http: HttpClient) {}
 
