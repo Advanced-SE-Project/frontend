644109d08b752d9fa35a5e74646327f0ebd27d5b
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2025-01-19 17:38:19 +0100

    routing

--- src/app/helpers/http.interceptor.ts
+++ src/app/helpers/http.interceptor.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.getToken();
-
+    const token = localStorage.getItem('jwt_token');
     if (token) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${token}`),
       });
       return next.handle(cloned);
     }
-
     return next.handle(req);
   }
 }
