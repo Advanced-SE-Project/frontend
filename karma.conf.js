eeb8f810bb468b2bc4420e47b153592c3af7dc35
Evangelia Panteliadou <106238947+evangeliapant@users.noreply.github.com>
2025-01-22 23:45:45 +0100

    update karma and CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,13 @@
-module.exports = function(config) {
-    config.set({
-      browsers: ['ChromeHeadless'],
-      // other configurations
-    });
-  };
-  
+module.exports = function (config) {
+  config.set({
+    browsers: ['ChromeHeadless'], // Use headless Chrome
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-extensions', '--disable-dev-shm-usage'],
+      },
+    },
+    singleRun: true, // Ensures Karma exits after tests
+    // other configurations...
+  });
+};
